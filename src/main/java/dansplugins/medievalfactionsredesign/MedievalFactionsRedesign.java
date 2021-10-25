cd7eb33df1586a6118a97f5236c7fc7f7ec416f8
dmccoystephenson <dmccoystephenson@gmail.com>
2021-10-25 18:44:33 -0400

    initial commit

---
+++ src/main/java/dansplugins/medievalfactionsredesign/MedievalFactionsRedesign.java
@@ -0,0 +1,16 @@
+package dansplugins.medievalfactionsredesign;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MedievalFactionsRedesign extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
