da88545c3035e909410cab0ccc49a69a88a0bc16
dmccoystephenson <dmccoystephenson@gmail.com>
2022-04-13 23:59:41 -0600

    Added the kick command.

---
+++ src/main/java/dansplugins/minifactions/commands/KickCommand.java
@@ -0,0 +1,72 @@
+package dansplugins.minifactions.commands;
+
+import org.bukkit.command.CommandSender;
+
+import dansplugins.minifactions.api.definitions.core.Faction;
+import dansplugins.minifactions.api.definitions.core.FactionPlayer;
+import dansplugins.minifactions.api.exceptions.CommandSenderNotPlayerException;
+import dansplugins.minifactions.api.exceptions.FactionNotFoundException;
+import dansplugins.minifactions.commands.abs.AbstractMFCommand;
+import preponderous.ponder.minecraft.bukkit.tools.UUIDChecker;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.UUID;
+
+/**
+ * @author Daniel McCoy Stephenson
+ */
+public class InviteCommand extends AbstractMFCommand {
+
+    public InviteCommand() {
+        super(new ArrayList<>(Arrays.asList("invite")), new ArrayList<>(Arrays.asList("mf.invite")));
+    }
+
+    @Override
+    public boolean execute(CommandSender commandSender) {
+        commandSender.sendMessage("Usage: /mf invite <IGN>");
+        return true;
+    }
+
+    @Override
+    public boolean execute(CommandSender commandSender, String[] args) {
+        FactionPlayer player;
+        try {
+            player = getFactionPlayer(commandSender);
+        } catch(CommandSenderNotPlayerException e) {
+            return false;
+        }
+
+        Faction faction;
+        try {
+            faction = getAPI().getFactionByPlayer(player);
+        } catch(FactionNotFoundException e) {
+            player.sendMessage("You are not in a faction.");
+            return false;
+        }
+
+        if (!player.getId().equals(faction.getLeader())) {
+            player.sendMessage("You are not the leader of your faction.");
+            return false;
+        }
+
+        String ign = args[0];
+
+        UUIDChecker uuidChecker = new UUIDChecker();
+        UUID targetUUID = uuidChecker.findUUIDBasedOnPlayerName(ign);
+
+        if (targetUUID.equals(player.getId())) {
+            player.sendMessage("You cannot kick yourself.");
+            return false;
+        }
+
+        boolean success = faction.removeMember(targetUUID);
+        if (success) {
+            player.sendMessage("That player has been kicked.");
+        }
+        else {
+            player.sendMessage("That player wasn't found.");
+        }
+        return success;
+    }
+}
